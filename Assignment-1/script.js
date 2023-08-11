11322b86958e558bcf19ee864b4cfa563337497d
Akshaya.kcoddle <akshaya.kcoddletech.com>
2023-08-11 21:58:47 +0530

    Assigment-1

---
+++ Assignment-1/script.js
@@ -0,0 +1,19 @@
+// variables with different data types
+const name = 'akshaya';
+let middleName;
+const lastName = 'paloran';
+const age = 25;
+const isCalculated = true;
+const passWord = null;
+// data types along with each variable name in the console.
+/* eslint-disable no-console */
+console.log(`name :${typeof name}`);
+console.log(`middleName :${typeof middleName}`);
+console.log(`Last-Name : '${typeof lastName}`);
+console.log(`age : ${typeof age}`);
+console.log(`IS-Calculated : ${typeof isCalculated}`);
+console.log(`Password : ${typeof passWord}`);
+// concat string with the above var without using template literals
+console.log('Full Name :', name, '', lastName);
+// concat string with the above var using template literals
+console.log(`Full Name : ${name} ${lastName}`);
