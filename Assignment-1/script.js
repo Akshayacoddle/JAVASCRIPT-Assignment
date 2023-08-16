57e9a349fdd4125ff9586ff371ce78cd2f621eb6
Jishnu <jishnu@CT129-JISHNURAJ-K.local>
2023-08-16 10:36:02 +0530

    updated crction

--- Assignment-1/script.js
+++ Assignment-1/script.js
@@ -1,12 +1,14 @@
 // variables with different data types
-const name = 'akshaya';
+//check any data type is missing
+const name = "akshaya";
 let middleName;
-const lastName = 'paloran';
+const lastName = "paloran";
 const age = 25;
 const isCalculated = true;
 const passWord = null;
 // data types along with each variable name in the console.
 /* eslint-disable no-console */
+//specify the datatypes in the console
 console.log(`name :${typeof name}`);
 console.log(`middleName :${typeof middleName}`);
 console.log(`Last-Name : '${typeof lastName}`);
@@ -14,6 +16,6 @@ console.log(`age : ${typeof age}`);
 console.log(`IS-Calculated : ${typeof isCalculated}`);
 console.log(`Password : ${typeof passWord}`);
 // concat string with the above var without using template literals
-console.log('Full Name :', name, '', lastName);
+console.log("Full Name :", name, "", lastName);
 // concat string with the above var using template literals
 console.log(`Full Name : ${name} ${lastName}`);
--- Assignment-2/script-2.js
+++ Assignment-2/script-2.js
@@ -1,7 +1,7 @@
-const arr = ['1', '2', '3', '4', '5', '6', '7'];
+const arr = ["1", "2", "3", "4", "5", "6", "7"];
 /* eslint-disable no-console */
 //  a) Remove number "6"
-console.log(arr.splice(arr.indexOf('6'), 1));
+console.log(arr.splice(arr.indexOf("6"), 1));
 // a) after removing length of the array
 console.log(arr.length);
 // b) Convert all the items of the array to data type number
@@ -9,18 +9,20 @@ arr.forEach((value) => {
   console.log(value, typeof Number(value));
 });
 // c)  Remove last three items of the array
+//do it with single operation
 arr.pop();
 arr.pop();
 arr.pop();
 // c) array after removing last 3
 console.log(arr);
-arr.unshift('two');
-arr.unshift('one');
+//do it with single operation
+arr.unshift("two");
+arr.unshift("one");
 // c) array after adding 1,2 at begining
 console.log(arr);
 // d) string concatenation of all items of the array
-const arr1 = ['1', '2', '3', '4', '5', '6', '7'];
-let stringConcat = '';
+const arr1 = ["1", "2", "3", "4", "5", "6", "7"];
+let stringConcat = "";
 let sumOfArray = 0;
 arr1.forEach((value) => {
   stringConcat += value;
@@ -31,18 +33,20 @@ console.log(stringConcat);
 // d)  sum of all the items
 console.log(sumOfArray);
 // e)  Filter out item "3"
-const filtered = arr.filter((value) => value === '3');
+const filtered = arr.filter((value) => value === "3");
 console.log(filtered);
 console.log(arr);
 // f) Iterate the array and console the item, when item is either "3", "6" or "7"
 arr1.forEach((value) => {
-  if (value === '3' || value === '6' || value === '7') {
+  if (value === "3" || value === "6" || value === "7") {
     console.log(value);
   }
 });
 // g)Compare new array with the above given array if both have same data type
-const arrNew = [1, 2, '3', 4, 5, 6, '7'];
-const arrOld = ['1', '2', '3', '4', '5', '6', '7'];
+//Read the question carefully.
+// compare new array with old array
+const arrNew = [1, 2, "3", 4, 5, 6, "7"];
+const arrOld = ["1", "2", "3", "4", "5", "6", "7"];
 for (let i = 0; i < arrOld.length; i += 1) {
   if (typeof arrNew[i] === typeof arrOld[i]) {
     console.log(arrNew[i], typeof arrNew[i]);
@@ -62,14 +66,14 @@ const mergeArray = firstArray.concat(secondtArray);
 console.log(mergeArray);
 // star pattern
 const n = 5;
-let string = '';
+let string = "";
 for (let i = 1; i <= n; i += 1) {
   for (let k = 1; k <= n - i; k += 1) {
-    string += ' ';
+    string += " ";
   }
   for (let j = 1; j <= i; j += 1) {
-    string += '* ';
+    string += "* ";
   }
-  string += '\n';
+  string += "\n";
 }
 console.log(string);
