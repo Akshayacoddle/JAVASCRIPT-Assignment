a20c9bd0ec46f7ce5c45fe9474935bcbd48bb643
Akshaya.kcoddle <akshaya.kcoddletech.com>
2023-09-01 15:50:40 +0530

    corrected

--- Assignment-5/script.js
+++ Assignment-5/script.js
@@ -1,48 +1,105 @@
 // eslint-disable-next-line no-unused-vars
-function submitForm() {
-  let Err = true;
-  // eslint-disable-next-line no-restricted-globals
-  event.preventDefault();
-  const regex = /^[1-9]\d{5}$/;
+// eslint-disable-next-line no-restricted-globals
+let errName = false;
+let errMob = false;
+let errPlace = false;
+let errCompany = false;
+let errpin = false;
+const form = document.getElementById('form');
+const name = document.getElementById('name');
+const mobile = document.getElementById('mobile');
+const place = document.getElementById('place');
+const company = document.getElementById('company');
+const pin = document.getElementById('pin');
+const nameId = document.getElementById('name').id;
+const mobileNumId = document.getElementById('mobile').id;
+const placeId = document.getElementById('place').id;
+const companyNameId = document.getElementById('company').id;
+const pinCodeId = document.getElementById('pin').id;
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  // eslint-disable-next-line no-use-before-define
+  validateInput();
+});
+const setError = (element, message) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector('.error');
+  errorDisplay.innerHTML = message;
+  inputControl.classList.add('error');
+  inputControl.classList.remove('success');
+};
+/* a) if any of the fields are empty on submitting it should show
+corresponding error messages below all the required fields. */
+const setSuccess = ele => {
+  const inputControl = ele.parentElement;
+  const errorDisplay = inputControl.querySelector('.error');
+  errorDisplay.innerHTML = '';
+  inputControl.classList.remove('error');
+  inputControl.classList.add('success');
+};
+const isValidMobile = (mob) => {
   const regex1 = /^[1-9]\d{9}$/;
-  const name = document.getElementById('name').value;
-  const mobileNum = document.getElementById('mobile').value;
-  const place = document.getElementById('place').value;
-  const companyName = document.getElementById('company').value;
-  const pinCode = document.getElementById('pin').value;
-  const nameId = document.getElementById('name').id;
-  const mobileNumId = document.getElementById('mobile').id;
-  const placeId = document.getElementById('place').id;
-  const companyNameId = document.getElementById('company').id;
-  const pinCodeId = document.getElementById('pin').id;
-  /* a) if any of the fields are empty on submitting it should show
-  corresponding error messages below all the required fields. */
-  if (name === '' || place === '' || companyName === '') {
-    // eslint-disable-next-line no-alert
-    alert('below all the required fields! please fill it out');
-    // d) store the details in the local storage and clear the form
-  } else if (regex1.test(mobileNum) === false) {
-    document.getElementById('pindiv').innerHTML = '';
-    document.getElementById('namediv').innerHTML = 'please enter valid number .must have 10-digit';
-  }
-  else if (regex.test(pinCode) === false) {
-    document.getElementById('namediv').innerHTML = '';
-    document.getElementById('pindiv').innerHTML = 'please enter valid 6-digit Pin';
+  return regex1.test(mob);
+};
+const isValidPin = (pins) => {
+  const regex = /^[1-9]\d{5}$/;
+  return regex.test(pins);
+};
+
+const validateInput = () => {
+  const nameValue = name.value.trim();
+  const mobileValue = mobile.value.trim();
+  const placeValue = place.value.trim();
+  const companyValue = company.value.trim();
+  const pinValue = pin.value.trim();
+
+  if (nameValue === '') {
+    setError(name, 'username is required');
+    errName = false;
   } else {
-    document.getElementById('namediv').innerHTML = '';
-    document.getElementById('pindiv').innerHTML = '';
-    localStorage.setItem(nameId, name);
-    localStorage.setItem(mobileNumId, mobileNum);
-    localStorage.setItem(placeId, place);
-    localStorage.setItem(companyNameId, companyName);
-    localStorage.setItem(pinCodeId, pinCode);
+    setSuccess(name);
+    errName = true;
+  }
+  if (placeValue === '') {
+    setError(place, 'Place is required');
+    errPlace = false;
+  } else {
+    setSuccess(place);
+    errPlace = true;
+  }
+  if (companyValue === '') {
+    setError(company, 'Company Name is required');
+    errCompany = false;
+  } else {
+    setSuccess(company);
+    errCompany = true;
+  }
+  if (mobileValue === '' || (!isValidMobile(mobileValue))) {
+    errMob = false;
+    setError(mobile, 'please enter valid number .must have 10-digit');
+  } else {
+    setSuccess(mobile);
+    errMob = true;
+  } if (pinValue === '' || (!isValidPin(pinValue))) {
+    setError(pin, 'please enter valid 6-digit Pin');
+    errpin = false;
+  } else {
+    setSuccess(pin);
+    errpin = true;
+  }
+  if (errpin === true && errPlace === true && errCompany === true
+    && errName === true && errMob === true) {
+    localStorage.setItem(nameId, name.value);
+    localStorage.setItem(mobileNumId, mobile.value);
+    localStorage.setItem(placeId, place.value);
+    localStorage.setItem(companyNameId, company.value);
+    localStorage.setItem(pinCodeId, pin.value);
     // eslint-disable-next-line no-undef
     form.reset();
   }
-}
-
+};
 /* Make a prepopulate button, which when clicked will populate the form with values in the
- local storage if it exists, otherwise, the button will be disabled. */
+local storage if it exists, otherwise, the button will be disabled. */
 if (localStorage.getItem('name') === null) {
   // eslint-disable-next-line no-console
   console.log('novalue');
@@ -63,6 +120,7 @@ if (localStorage.getItem('name') === null) {
     document.getElementById('pin').value = localStorage.getItem('pin');
   });
 }
+
 /* 2 )Create a button and div using JS, when clicked on the button your basic details should be
 shown in the div.The static HTML file should only contain the basic HTML structure, no div/buttons.
 */
